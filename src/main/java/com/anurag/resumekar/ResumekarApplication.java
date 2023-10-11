2293d0117d0e2f5d644132957bd2596943608bef
anuragsknsits <anuragsknsits@gmail.com>
2023-10-12 00:43:18 +0530

    Initial Commit.

---
+++ src/main/java/com/anurag/resumekar/ResumekarApplication.java
@@ -0,0 +1,13 @@
+package com.anurag.resumekar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResumekarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResumekarApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/anurag/resumekar/ResumekarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anurag.resumekar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResumekarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
